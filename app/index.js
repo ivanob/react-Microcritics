8a150d856db3fe95dc770c5849aeb676368b668a
Ivan Obeso Agüera <ivan.obeso1@gmail.com>
2016-11-30 23:27:57 +0000

    some improvs in the menu

--- app/index.js
+++ app/index.js
@@ -4,16 +4,26 @@ var ReactDOM = require('react-dom');
 var PortraitContainer = require('./containers/PortraitContainer');
 var Appbar = require('muicss/lib/react/appbar');
 var createStore = require('redux')
-var Provider = require('react-redux')
+//var Provider = require('react-redux')
+var Input = require('muicss/lib/react/input')
+var Button = require('muicss/lib/react/button')
 
-let store = createStore(todoApp)
-
+//let store = createStore(App)
+let s1 = {verticalAlign: 'middle'};
 const App = () => (
+
   <div>
-  <Provider store={store}>
-    <Appbar></Appbar>
+    <Appbar>
+    <table width="100%">
+         <tbody>
+           <tr style={s1}>
+              <td className="mui--appbar-height" width="30%">  <Input hint="Title"/> </td>
+              <td className="mui--appbar-height">  <Button color="primary">Find</Button> </td>
+           </tr>
+          </tbody>
+      </table>
+    </Appbar>
     <PortraitContainer/>
-  </Provider>
   </div>
 );
 
